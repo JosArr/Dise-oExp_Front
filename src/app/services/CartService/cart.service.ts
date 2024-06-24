a78d11b03c70f77091503b694590dc43ede90be6
JosArr <josuedavidarrunategui@gmail.com>
2024-06-23 21:23:46 -0500

    update cart Service

--- src/app/services/CartService/cart.service.ts
+++ src/app/services/CartService/cart.service.ts
@@ -10,7 +10,7 @@ export class CartService {
   constructor() { }
 
   addToCart(food: any){
-    this.items.push(food);
+    this.items.push({...food, quantity: 1});
   }
 
   getItems(){
@@ -19,5 +19,34 @@ export class CartService {
 
   delete(item: any) {
     this.items = this.items.filter((i) => i.id !== item.id);
+  }
+
+  incrementQuantity(id: number){
+    let food = this.items.find((i) => i.id === id);
+    if(food){
+      food.quantity++;
+    }
+  }
+
+  decrementQuantity(id: number){
+    let food = this.items.find((i) => i.id === id);
+    if(food){
+      food.quantity--;
+      if(food.quantity === 0){
+        this.delete(food);
+      }
+    }
+  }
+
+  getTotal(){
+    return this.items.reduce((total, item) => {
+      return total + (item.price * item.quantity);
+    }, 0);
+  }
+
+  getTotalCal(){
+    return this.items.reduce((total, item) => {
+      return total + (item.calories * item.quantity);
+      }, 0);
   }
 }
